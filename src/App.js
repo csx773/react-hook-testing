a6c37fe1b4188dc5809961682e1eb80f89c641e8
Chris Xiao <csx773@gmail.com>
2019-09-13 21:41:18 +0000

    trying to fix hot reload not working

--- src/App.js
+++ src/App.js
@@ -11,7 +11,11 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Testing to implement some react hooks technique. Hot reload working? 
+        </p>
+        <br></br>
+        <p className="App-intro">
+          why is hot reload not working here?
         </p>
       </div>
     );
